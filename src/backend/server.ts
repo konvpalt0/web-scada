2e0f0d0cfd02e6034595a4eba87688d3773dabb4
Nwors <8grisha8@mail.ru>
2021-02-21 16:34:58 +0500

    Get connection to local database

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,11 +1,24 @@
 import {Response, Request} from 'express'
 
 const app = require('express')()
-const pgp = require('pg-promise')
-let db = pgp('postgres://username:password@host:port/database')
+const pgp = require('pg-promise')({})
+
+var cn = {
+    host: 'localhost', // 'localhost' is the default
+    port: 5432, // 5432 is the default;
+    database: 'webscada',
+    user: 'operator',
+    password: 'operator'
+};
+const db = pgp(cn)
 
 const port = 3001
+
 app.get('/api', function(req: Request, res: Response){
+    let result = db.any('SELECT * from Users').then((value: JSON) => {
+        console.log()
+    })
+    console.log(result);
     res.json([
         {
             id: 1, username: "somebody"
